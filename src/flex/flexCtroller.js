0e0bda360c5e831439d597d0acdb2b210f9fac2c
cwx280216 <chenxiaohui4@huawei.com>
2017-05-17 11:14:55 +0800

    add src

---
+++ src/flex/flexCtroller.js
@@ -0,0 +1,9 @@
+/**
+ * Created by cWX280216 on 2016/12/30.
+ */
+angular.module('flexApp',['ui.router'])
+    .controller('flexCtroller', function ($scope) {
+        //$scope.imageList=["img1","img2","img3","img4","img5","img6","img7","img8","img9"];
+        
+    })
+    
